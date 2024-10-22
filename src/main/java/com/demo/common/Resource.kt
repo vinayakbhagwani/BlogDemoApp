e2a7cf27f992dcc37aa0b5ef28bed72f3990bd93
Vinayak Bhagwani <vinayakbhagwani@gmail.com>
2024-10-22 08:27:11 +0530

    First Commit in common

---
+++ src/main/java/com/demo/common/Resource.kt
@@ -0,0 +1,8 @@
+package com.demo.common
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+
+    class Success<T>(data: T?) : Resource<T>(data = data)
+    class Loading<T>(message: String?) : Resource<T>()
+    class Error<T>(message: String?) : Resource<T>(message = message)
+}
